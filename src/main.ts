db4b33e5622d2e6c4f2dfdd217c6ae41c69fa2c0
Efim Belyakov <belyakov.efim@gmail.com>
2021-12-04 01:01:39 +0300

    fix ci/cd test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log("Start APP");
+  console.log("Start APP!");
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
